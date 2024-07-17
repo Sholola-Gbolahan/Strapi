c7a2f55d56848ba06c9ad06a2283f5fbdebb90e6
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-17 01:20:01 +0100

    Logic setup to get pageId from global context

--- src/Context.jsx
+++ src/Context.jsx
@@ -5,6 +5,7 @@ export const useGlobalContext = () => useContext(AppContext)
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [pageId, setPageId] = useState(null)
   const closeSidebar = () => {
     setIsSidebarOpen(false)
   }
@@ -13,7 +14,9 @@ const AppProvider = ({ children }) => {
     setIsSidebarOpen(true)
   }
   return (
-    <AppContext.Provider value={{ isSidebarOpen, closeSidebar, openSidebar }}>
+    <AppContext.Provider
+      value={{ isSidebarOpen, closeSidebar, openSidebar, pageId, setPageId }}
+    >
       {children}
     </AppContext.Provider>
   )
--- src/NavLinks.jsx
+++ src/NavLinks.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import sublinks from "./data"
+import { useGlobalContext } from "./Context"
 
 const NavLinks = () => {
+  const { pageId, setPageId } = useGlobalContext()
   return (
     <div className="nav-links">
       {sublinks.map(({ pageId, page }) => {
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+            // onMouseLeave={() => setPageId(null)}
+          >
             {page}
           </button>
         )
